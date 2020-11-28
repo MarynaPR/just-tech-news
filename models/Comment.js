0a2c0dcb4b210b94788a02f2c9585e4e886cfb14
MarynaPR <pryadkamaryna@gmail.com>
2020-11-28 11:48:21 -0600

    commit before swithing to another branch

--- models/Comment.js
+++ models/Comment.js
@@ -10,6 +10,14 @@ Comment.init(
             primaryKey: true,
             autoIncrement: true
         },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the comment must be at least one characters long
+                len: [1]
+            }
+        },
         user_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
@@ -24,10 +32,6 @@ Comment.init(
             references: {
                 model: 'post',
                 key: 'id'
-            },
-            validate: {
-                // this means the comment must be at least one characters long
-                len: [1]
             }
         }
     },
