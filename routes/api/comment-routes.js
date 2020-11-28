eb0db8d4afd4c26d2e05942dea60c3c26d000160
MarynaPR <pryadkamaryna@gmail.com>
2020-11-28 13:19:04 -0600

    comment posts added

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
@@ -16,11 +21,25 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(400).json(err);
         });
-
 });
 
 router.delete('/:id', (req, res) => {
-
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
