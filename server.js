ce2682e2b80cda15ad3218e4b8a52563d283f971
MarynaPR <pryadkamaryna@gmail.com>
2020-12-06 20:22:03 -0600

    partials added

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -20,8 +19,8 @@ const sess = {
 };
 
 app.use(session(sess));
-const hbs = exphbs.create({});
-
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
